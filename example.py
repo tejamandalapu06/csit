95726914f08ea8068f3e7b151837d896a534a3a0
Srivs8119 <srivishnumolakala2005h@gmail.com>
2025-07-01 10:51:00 +0530

    Update example.py

--- example.py
+++ example.py
@@ -1,7 +1,7 @@
 a=[1,2,1,1,2,3,2,4,5,6,5,4,73,8,8,99,7,8,6,9]
 n=len(a)
-max=a[0]
+min=a[0]
 for i in range(n):
-    if(max<a[i]):
-        max=a[i]
-print(max)
+    if(min>a[i]):
+        min=a[i]
+print(min)
